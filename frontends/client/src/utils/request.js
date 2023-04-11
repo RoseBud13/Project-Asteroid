6e6d93dfcbb30d37aa59c9bcace27af473908e29
RoseBud13 <xiongkaijie13@gmail.com>
2023-04-11 20:19:48 +0800

    fix: update the api url of video wallpaper

--- frontends/client/src/utils/request.js
+++ frontends/client/src/utils/request.js
@@ -18,18 +18,21 @@ export async function fetchOneWallpaperUrl() {
 
 export async function fetchMoonshinerUrl() {
   try {
-    const response = await fetch('https://b612.one/static/moonshiner.json', {
-      headers: {
-        'Content-Type': 'application/json'
+    const response = await fetch(
+      'https://b612.one/static-rsrc/moonshiner.json',
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
       }
-    });
+    );
     if (!response.ok) {
       // throw new Error("Network response was not OK");
       console.error('"Network response was not OK');
       return [];
     }
     const jsonData = await response.json();
-    console.log(jsonData.data);
+    // console.log(jsonData.data);
     return jsonData.data;
   } catch (error) {
     console.error('There has been a problem with your fetch operation:', error);
