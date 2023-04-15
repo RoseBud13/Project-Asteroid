1b39190831e75b082b050542b9e70cfd1fa71db1
RoseBud13 <xiongkaijie13@gmail.com>
2023-04-15 10:39:50 +0800

    feat: made useDraggable function a reusable utility

--- frontends/client/src/components/applications/notes/index.vue
+++ frontends/client/src/components/applications/notes/index.vue
@@ -1,66 +1,19 @@
 <template>
-  <div
-    class="astra-notes-container"
-    :style="{
-      top: `${originPosition.y}px`,
-      left: `${originPosition.x}px`,
-      transform: `translate(${elementOffset.x}px, ${elementOffset.y}px)`
-    }"
-  >
+  <div class="astra-notes-container" :style="style">
     <div ref="notesHeader" class="notes-header"></div>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive, onMounted, onBeforeUnmount } from 'vue';
+import { ref, onBeforeUnmount } from 'vue';
+import { useDraggable } from '@/utils/elements';
+import { Local } from '@/utils/storage';
 
 const notesHeader = ref(null);
+const notesPosition = Local.get('notes-position');
 
-const originPosition = reactive({
-  x: 50,
-  y: 200
-});
-
-const elementOffset = reactive({
-  x: 0,
-  y: 0
-});
-
-const mousedownOffset = reactive({
-  x: 0,
-  y: 0
-});
-
-const updatePosition = () => {
-  originPosition.x = originPosition.x + elementOffset.x;
-  originPosition.y = originPosition.y + elementOffset.y;
-  elementOffset.x = 0;
-  elementOffset.y = 0;
-};
-
-const handleMousedown = event => {
-  event.stopPropagation();
-  mousedownOffset.x = event.clientX - originPosition.x;
-  mousedownOffset.y = event.clientY - originPosition.y;
-  document.addEventListener('mousemove', handleMousemove, true);
-  document.addEventListener('mouseup', handleMouseup, true);
-};
-
-const handleMousemove = event => {
-  event.stopPropagation();
-  elementOffset.x = event.clientX - originPosition.x - mousedownOffset.x;
-  elementOffset.y = event.clientY - originPosition.y - mousedownOffset.y;
-};
-
-const handleMouseup = event => {
-  event.stopPropagation();
-  document.removeEventListener('mousemove', handleMousemove, true);
-  document.removeEventListener('mouseup', handleMouseup, true);
-  updatePosition();
-};
-
-onMounted(() => {
-  notesHeader.value.addEventListener('mousedown', handleMousedown, true);
+const { style, handleMousedown } = useDraggable(notesHeader, {
+  initPosition: notesPosition
 });
 
 onBeforeUnmount(() => {
@@ -76,7 +29,7 @@ export default {
 
 <style lang="scss" scoped>
 .astra-notes-container {
-  position: absolute;
+  position: fixed;
   // top: 50%;
   // left: 50%;
   // transform: translate(-50%, -50%);
@@ -93,5 +46,10 @@ export default {
   position: relative;
   width: 100%;
   height: 40px;
+  border-radius: 15px 15px 0 0;
+}
+
+.notes-header:hover {
+  background-color: var(--color-neutral-1);
 }
 </style>
