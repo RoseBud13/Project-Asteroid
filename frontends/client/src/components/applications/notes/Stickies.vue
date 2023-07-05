c1927f014cf5f80c3dbe34b392b015d8abd0e5e4
RoseBud13 <xiongkaijie13@gmail.com>
2023-07-05 17:48:45 +0800

    feat: add style to note stickies

--- frontends/client/src/components/applications/notes/Stickies.vue
+++ frontends/client/src/components/applications/notes/Stickies.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="stickies">
-    <h3>{{ noteID }}</h3>
-    <p>{{ noteContent }}</p>
-    <div @click="handleUnpinStickies(noteID)">X</div>
+  <div class="sticky-box">
+    <div class="sticky-header">
+      <h3>{{ noteID }}</h3>
+    </div>
+    <div class="sticky-body">
+      <p>{{ noteContent }}</p>
+    </div>
+    <div class="sticky-footer">
+      <div class="unpin-btn">
+        <IconPushpinOff @click="handleUnpinStickies(noteID)"></IconPushpinOff>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useAppNotesStore } from '@/stores/appNotes';
 import { storeToRefs } from 'pinia';
+import IconPushpinOff from '@/components/icons/IconPushpinOff.vue';
 
 const appNotesStore = useAppNotesStore();
 const { stickyList } = storeToRefs(appNotesStore);
@@ -31,18 +40,50 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.stickies-wrapper {
-  position: absolute;
-  top: 100px;
-  left: 100px;
-  width: 200px;
-  min-height: 100px;
-}
-
-.stickies {
+<style lang="scss" scoped>
+.sticky-box {
+  position: relative;
   width: 100%;
   height: 100%;
-  background-color: #fefefe;
+  background-color: #ffffb7;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.sticky-header {
+  position: relative;
+  width: 100%;
+  height: 30px;
+  padding: 5px 10px;
+}
+
+.sticky-body {
+  flex: 1;
+  width: 100%;
+  padding: 0 10px;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+.sticky-body p {
+  white-space: pre-line;
+}
+
+.sticky-footer {
+  position: relative;
+  width: 100%;
+  height: 30px;
+  padding: 5px 10px;
+  display: flex;
+  flex-direction: row-reverse;
+}
+
+.unpin-btn {
+  position: relative;
+  font-size: 15px;
+  padding: 0 10px;
+  cursor: pointer;
 }
 </style>
