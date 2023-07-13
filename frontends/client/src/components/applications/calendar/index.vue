dee65b69361c5a9506edea4d01c8bceb6833edb4
RoseBud13 <xiongkaijie13@gmail.com>
2023-07-13 16:26:55 +0800

    feat: init weather and calendar components

---
+++ frontends/client/src/components/applications/calendar/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script setup></script>
+
+<script>
+export default {
+  name: 'AstraCalendar'
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontends/client/src/components/applications/weather/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script setup></script>
+
+<script>
+export default {
+  name: 'AstraWeather'
+};
+</script>
+
+<style lang="scss" scoped></style>
